2264b3ff4b63bb74706509c23be124d19af390b4
Téo G. <183733003+zeyluish@users.noreply.github.com>
2026-08-28 15:51:38 +0200

    feat(database): Liaison de la base de donnée Supabase + Création de la methode POST - Contact

--- server.js
+++ server.js
@@ -1,19 +1,31 @@
 import express from "express";
+import dotenv from "dotenv";
+import { createClient } from "@supabase/supabase-js";
+
+dotenv.config();
 
 const app = express();
 const PORT = 3000;
 
-// Sert les fichiers statiques du dossier 'public'
-app.use(express.static("public"));
+// Supabase
+const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY);
 
-// Middleware JSON
+app.use(express.static("public"));
 app.use(express.json());
 
-// Route de test
-app.get("/", (req, res) => {
-  res.sendFile("public/index.html");
+// POST - Contact
+app.post("/api/contact", async (req, res) => {
+  const { nom, prenom, telephone, email, objet, message } = req.body;
+
+  const { error } = await supabase.from("messages_contact").insert([{ nom, prenom, telephone, email, objet, message }]);
+
+  if (error) {
+    return res.status(400).json({ error: error.message });
+  }
+
+  res.json({ success: true });
 });
 
 app.listen(PORT, () => {
-  console.log(`🚀 Serveur lancé sur http://localhost:${PORT}`);
+  console.log(`🚀 Serveur sur http://localhost:${PORT}`);
 });
